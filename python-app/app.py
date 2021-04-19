1dcb5c54a0e37bed750e1b7e55b05dd8ce5250e5
barywhyte <seunsamore@gmail.com>
2021-04-19 02:34:23 +0100

    reworked api gateway get method

--- python-app/app.py
+++ python-app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/")
 def hello():
-    return "Flask with API Gateway!"
+    return "Flask inside Docker!!"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
